aa9de0de29f48f153a706e6b1e0c79edd7c8d8b3
dlals711 <68318350+dlals711@users.noreply.github.com>
2021-01-15 20:12:43 +0900

    간단한 게시판

--- 스프링 퀵 스타트/pro27/src/main/java/com/myspring/view/user/LoginController.java
+++ 스프링 퀵 스타트/pro27/src/main/java/com/myspring/view/user/LoginController.java
@@ -3,6 +3,7 @@ package com.myspring.view.user;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,26 +13,24 @@ import com.myspring.pro27.user.impl.UserDAO;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/login.do", method=RequestMethod.GET)
-	public String loginView(UserVO vo) {
-		System.out.println("로그인 화면으로 이동");
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String loginView(@ModelAttribute("user") UserVO vo) {
+		System.out.println("로그인 화면으로 이동...");
 		vo.setId("test");
 		vo.setPassword("test123");
 		return "login.jsp";
 	}
-	
-	@RequestMapping(value="/login.do", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
 	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
-		if(vo.getId() == null || vo.getId().equals("")) {
+		if (vo.getId() == null || vo.getId().equals("")) {
 			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
 		}
 		UserVO user = userDAO.getUser(vo);
-		if(user != null) {
+		if (user != null) {
 			session.setAttribute("userName", user.getName());
 			return "getBoardList.do";
-		} else {
+		} else
 			return "login.jsp";
-		}
 	}
-
 }
