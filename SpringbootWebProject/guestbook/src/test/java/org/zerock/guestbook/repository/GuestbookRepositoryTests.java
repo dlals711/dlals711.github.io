ff382423bd13cd1cbfe83b2fd61bf973bb3cafe4
dlals711 <68318350+dlals711@users.noreply.github.com>
2021-01-23 17:08:55 +0900

    SBWP~p220

--- SpringbootWebProject/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ SpringbootWebProject/guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -17,30 +17,35 @@ import java.util.stream.IntStream;
 
 @SpringBootTest
 public class GuestbookRepositoryTests {
+
     @Autowired
     private GuestbookRepository guestbookRepository;
 
     @Test
-    public void insertDummies() {
+    public void insertDummies(){
+
         IntStream.rangeClosed(1,300).forEach(i -> {
+
             Guestbook guestbook = Guestbook.builder()
-                                            .title("Title...."+i)
-                                            .content("Content..."+i)
-                                            .writer("user" + (i % 10))
-                                            .build();
-                                    System.out.println(guestbookRepository.save(guestbook));
+                    .title("Title...." + i)
+                    .content("Content..." +i)
+                    .writer("user" + (i % 10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
         });
     }
 
     @Test
     public void updateTest() {
-        Optional<Guestbook> result = guestbookRepository.findById(300L); // 존재하는 번호로 테스트
 
-        if(result.isPresent()) {
+        Optional<Guestbook> result = guestbookRepository.findById(300L); //존재하는 번호로 테스트
+
+        if(result.isPresent()){
+
             Guestbook guestbook = result.get();
 
             guestbook.changeTitle("Changed Title....");
-            guestbook.changeContent("Changed Content....");
+            guestbook.changeContent("Changed Content...");
 
             guestbookRepository.save(guestbook);
         }
@@ -48,27 +53,30 @@ public class GuestbookRepositoryTests {
 
     @Test
     public void testQuery1() {
+
         Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
 
-        QGuestbook qGuestbook = QGuestbook.guestbook; // 1
+        QGuestbook qGuestbook = QGuestbook.guestbook; //1
 
         String keyword = "1";
 
-        BooleanBuilder builder = new BooleanBuilder(); // 2
+        BooleanBuilder builder = new BooleanBuilder();  //2
 
-        BooleanExpression expression = qGuestbook.title.contains(keyword); // 3
+        BooleanExpression expression = qGuestbook.title.contains(keyword); //3
 
-        builder.and(expression); // 4
+        builder.and(expression); //4
 
-        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); // 5
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); //5
 
         result.stream().forEach(guestbook -> {
             System.out.println(guestbook);
         });
+
     }
 
     @Test
     public void testQuery2() {
+
         Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
 
         QGuestbook qGuestbook = QGuestbook.guestbook;
@@ -77,21 +85,22 @@ public class GuestbookRepositoryTests {
 
         BooleanBuilder builder = new BooleanBuilder();
 
-        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exTitle =  qGuestbook.title.contains(keyword);
 
-        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exContent =  qGuestbook.content.contains(keyword);
 
-        BooleanExpression exAll = exTitle.or(exContent); // 1-----------
+        BooleanExpression exAll = exTitle.or(exContent); // 1----------------
 
-        builder.and(exAll); // 2-----
+        builder.and(exAll); //2-----
 
-        builder.and(qGuestbook.gno.gt(0L)); // 3-------
+        builder.and(qGuestbook.gno.gt(0L)); // 3-----------
 
         Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
 
         result.stream().forEach(guestbook -> {
             System.out.println(guestbook);
         });
+
     }
 
 }
--- SpringbootWebProject/guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ SpringbootWebProject/guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -1,11 +1,13 @@
 package org.zerock.guestbook.service;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
-
 
 @SpringBootTest
 public class GuestbookServiceTests {
@@ -25,4 +27,61 @@ public class GuestbookServiceTests {
         System.out.println(service.register(guestbookDTO));
 
     }
+
+//    @Test
+//    public void testList(){
+//
+//        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+//
+//        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+//
+//        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+//            System.out.println(guestbookDTO);
+//        }
+//    }
+
+
+    @Test
+    public void testList(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("-------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                                                    .page(1)
+                                                    .size(10)
+                                                    .type("tc") // 검색조건 t, c, w, tc, tcw ..
+                                                    .keyword("한글") // 검색 키워드
+                                                    .build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("-------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
 }
