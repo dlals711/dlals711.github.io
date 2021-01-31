ac75c42f883029c48a48b9d8ab37e120e498746a
dlals711 <68318350+dlals711@users.noreply.github.com>
2021-01-31 13:40:30 +0900

    익명게시판-1

---
+++ nonameboard/nnboard/src/main/java/org/zerock/nnboard/NnboardApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.nnboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnboardApplication.class, args);
+    }
+
+}
