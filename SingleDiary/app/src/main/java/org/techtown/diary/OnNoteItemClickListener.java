21257f8e09f28c2d7334968cf4bbdf916eebb45b
dlals711 <68318350+dlals711@users.noreply.github.com>
2021-02-20 02:59:43 +0900

    ~diary1

--- SingleDiary/app/src/main/java/org/techtown/diary/OnNoteItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.techtown.diary;
-
-import android.view.View;
-
-public interface OnNoteItemClickListener {
-    public void onItemClick(NoteAdapter.ViewHolder, View view, int position);
-}
--- SingleDiary/app/src/main/java/org/techtown/diary/OnTabItemSelectedListener.java
+++
@@ -1,5 +0,0 @@
-package org.techtown.diary;
-
-public interface OnTabItemSelectedListener {
-    public void onTabSelected(int position);
-}
